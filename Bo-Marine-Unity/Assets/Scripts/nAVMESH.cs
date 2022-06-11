ef6b61260794768c5eddee8c86cb1499caf09b63
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-06-11 12:04:02 +0200

    Gewerkt aan de AI. De animaties staan er ook in. Verder heb ik ook gewerkt aan de code voor de animator

--- Bo-Marine-Unity/Assets/Scripts/nAVMESH.cs
+++ Bo-Marine-Unity/Assets/Scripts/nAVMESH.cs
@@ -13,13 +13,10 @@ public class nAVMESH : MonoBehaviour
     private Animator animator;
 
     [Header("Patrolling")]
-    [SerializeField] Vector3 walkPoint;
-    [SerializeField] float walkPointRange;
     bool walkPointSet;
 
     [Header("Attacking")]
     [SerializeField] float timeBetweenAttacks;
-    //[SerializeField] GameObject projectile;
     bool alreadyAttacked;
 
     [Header("States")]
@@ -41,71 +38,61 @@ public class nAVMESH : MonoBehaviour
         //check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (!playerInSightRange && !playerInAttackRange) Idle();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
 
-    void Patrolling()
+    void Idle()
     {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
+        animator.Play("Idle");
     }
-    void SearchWalkPoint()
-    {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
-    }
+   
     void ChasePlayer()
     {
         agent.SetDestination(player.position);
-        animator.SetTrigger("walk animation"); 
+        animator.SetTrigger("Walking"); 
     }
     void AttackPlayer()
     {
         //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
         transform.LookAt(player);
+        animator.SetTrigger("Attack");
 
-        /*if (!alreadyAttacked)
+        if (!alreadyAttacked)
         {
             //attack code 
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            Rigidbody rb = Instantiate(animator,transform.position, Quaternion.identity).GetComponent<Rigidbody>();
             rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        }*/
+        }
     }
     void ResetAttack()
     {
         alreadyAttacked = false;
     }
 
-    public void TakeDamage(int damage)
+    public void TakeDamage(float damage)
     {
+        if (health <= 0)
+        {
+            return;
+        }
+
         health -= damage;
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+        if (health <= 0)
+        {
+            Death();
+        }
     }
 
-    private void DestroyEnemy()
+    private void Death()
     {
-        Destroy(gameObject);
+        animator.SetTrigger("Death");
     }
 
     private void OnDrawGizmosSelected()
