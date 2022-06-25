17bfb75906797fb9dc0cf9f66b43ecbe13eccec1
Mitchel Klijn <mitchelklijn2004@gmail.com>
2022-06-25 12:42:15 +0200

    Player health bar af en restart scene als je dood bent gegaan

--- Bo-Marine-Unity/Assets/Scripts/HealthPlayer.cs
+++ Bo-Marine-Unity/Assets/Scripts/HealthPlayer.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthPlayer : MonoBehaviour
 {
-    public float playerHealth = 5000f;
+    public Animator playerAnimator;
+    public Animator healthAnimator;
+    public GameObject healthBar;
+
+    public float playerHealth = 3f;
     public Transform target;
+
     public void TakeDamage(float amount)
     {
         playerHealth -= amount;
-        if (playerHealth <= 0f)
+        
+        if (playerHealth == 2f)
         {
-            Destroy(gameObject);
+            healthAnimator.SetTrigger("HighToMedium");
+        }
+        else if (playerHealth == 1f)
+        {
+            healthAnimator.SetTrigger("MediumToLow");
+        }
+        else
+        {
+            playerAnimator.SetTrigger("Dead");
+            healthAnimator.SetTrigger("Exit");
+            healthBar.SetActive(false);
+            StartCoroutine(Restart());
         }
     }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(10f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            TakeDamage(1f);
+        }
+    }*/
 }
