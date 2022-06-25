b44a64f0b27f14f444d5d6adeafce30725c71ac7
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-06-25 12:15:26 +0200

    Update camera voor de speler.

--- Bo-Marine-Unity/Assets/Scripts/CameraMovement.cs
+++ Bo-Marine-Unity/Assets/Scripts/CameraMovement.cs
@@ -4,35 +4,57 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform cameraPoint;
-    public float cameraSpeed = 100f;
+
+    [Header("Camera Settings")]
+    [SerializeField] float cameraSenX;
+    [SerializeField] float cameraSenY;
+
+    [Header("Transform References")]
+    [SerializeField] Transform followTransform;
+    public bool cursorVisible;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        cursorVisible = false;
     }
 
     void Update()
     {
-        Camera();
+        CameraRotation();
     }
 
-    void Camera()
+    void CameraRotation()
     {
-        float interpolation = cameraSpeed * Time.deltaTime;
+        //Get input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime;
+        float mouseY = -Input.GetAxis("Mouse Y") * Time.deltaTime;
 
-        Vector3 position = transform.position;
-        position.y = Mathf.Lerp(transform.position.y, cameraPoint.position.y, interpolation);
-        position.x = Mathf.Lerp(transform.position.x, cameraPoint.position.x, interpolation);
+        //Rotate the follow transform based on input
+        followTransform.transform.rotation *= Quaternion.AngleAxis(mouseX * cameraSenX, Vector3.up);
+        followTransform.transform.rotation *= Quaternion.AngleAxis(mouseY * cameraSenY, Vector3.right);
 
-        transform.position = position;
+        //Clamping looking Up/Down 
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
 
-        Quaternion quaternion = transform.rotation;
-        quaternion.eulerAngles = new Vector3(quaternion.eulerAngles.x, quaternion.eulerAngles.y, 0);
-        transform.rotation = quaternion;
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 330)
+        {
+            angles.x = 330;
+        }
+        else if (angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+        followTransform.transform.localEulerAngles = angles;
+
+        //Rotate player with camera
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+        //Reste the y rotation of look transform
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
     }
-    // Volledig rond het character draaien.
-    // Een limit voor het verticaal kijken.
-
 }
